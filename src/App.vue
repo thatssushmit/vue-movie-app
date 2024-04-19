843dede79f54a2c08e8a8f48a2a746085b98c1e2
Sush Mit <sushmit@Sushs-MacBook-Air.local>
2024-04-19 19:08:41 +0530

    updated minor chasnges to action and style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Movie List</router-link>
+    <nav class="neumorphism-navbar">
+      <router-link to="/" class="neumorphism-link">Movie List</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -15,4 +15,26 @@ export default {
 
 <style lang="scss">
 @import './assets/styles/main.scss';
+
+.neumorphism-navbar {
+  background-color: #f0f0f0;
+  border-radius: 20px 20px 0 0; /* Remove bottom corner radius */
+  padding: 10px 20px;
+  box-shadow: 10px 10px 20px #b7b7b7, -10px -10px 20px #ffffff;
+  margin-bottom: 20px;
+  text-align: center; /* Align text to center */
+  
+  .neumorphism-link {
+    text-decoration: none;
+    color: inherit;
+    font-weight: bold;
+    font-size: 18px;
+    text-shadow: 2px 2px 4px #ffffff, -2px -2px 4px #e0e0e0;
+    transition: transform 0.2s ease;
+
+    &:hover {
+      transform: translateY(-3px);
+    }
+  }
+}
 </style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,6 @@ export const fetchMovies = async ({ commit }, query) => {
       `https://www.omdbapi.com/?apikey=ca3b10e3&s=${query}`
     );
     commit('setMovies', response.data.Search);
-    commit('setCurrentComponent', 'MovieList'); 
   } catch (error) {
     console.error(error);
   }
@@ -18,7 +17,6 @@ export const fetchMovieDetails = async ({ commit }, movieId) => {
           `https://www.omdbapi.com/?apikey=ca3b10e3&i=${movieId}`
         );
         commit('setMovie', response.data);
-        commit('setCurrentComponent', 'MovieDetails'); 
       } catch (error) {
         console.error(error);
       }
